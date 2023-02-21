dbc0d0debe70807ea510b66c83dc58b91e3a4af8
samofoke <sabataernestmofokeng@gmail.com>
2023-02-21 13:10:01 +0200

    route for  Homapage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./routes/homepage/homepage";
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 };
 
 export default App;
