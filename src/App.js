776fc4745a2e9d102ad90fb34c91fd65dd5b1c41
samofoke <sabataernestmofokeng@gmail.com>
2023-02-25 18:57:41 +0200

    started with the sign-in page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation";
 import HomePage from "./routes/homepage/homepage";
+import SignIn from "./routes/sign-in/sign-in";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<HomePage />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -14,6 +14,9 @@ const Navigation = () => {
           <Link className="nav-link" to="/">
             Store
           </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
         </div>
       </div>
       <Outlet />
