885ab4641405cee6b71a98bcfb3fe57f68d44c67
samofoke <sabataernestmofokeng@gmail.com>
2024-04-12 15:51:59 +0200

    configuring the stare

--- src/index.js
+++ src/index.js
@@ -2,24 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Provider } from "react-redux";
 import { UserProvider } from "./contexts/user-context.";
 import { ProductProvider } from "./contexts/products-context";
 import { CartProvider } from "./contexts/cart/cart";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <ProductProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </ProductProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <ProductProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </ProductProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/store/store.js
+++ src/store/store.js
@@ -9,4 +9,10 @@ import { rooReducer } from "./root-reducer";
 // we going to need a root reducer to be our man reducer.
 //This will allows us to be able to generate a store.
 
-export const store = createStore(rooReducer);
+// going to build middlewares which will run before we trigger reduces.
+const middlewares = [logger];
+
+//this will allow us to enhance our middlewares and compose is functional concept.
+const composeEnhancers = compose(applyMiddleware(...middlewares));
+
+export const store = createStore(rooReducer, undefined, composeEnhancers);
